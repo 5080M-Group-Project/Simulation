6d1c6a401a5b9b93c7c5212c9123ac341c9f9c1e
sxin <xinsongyan@gmail.com>
2024-11-21 17:16:30 +0000

    add simple_robot.urdf

--- pybullet_simulation.py
+++ pybullet_simulation.py
@@ -4,16 +4,20 @@ import os
 import time
 
 pybullet.connect(pybullet.GUI)
-obj = pybullet.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), 0, 0, -1)
-subj = pybullet.loadURDF(os.path.join(os.getcwd(), "robot_description/urdf/robot_description.urdf"))
-pybullet.setGravity(0,0,-9.8)
+plane_id = pybullet.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), 0, 0, 0)
+robot_id = pybullet.loadURDF(os.path.join(os.getcwd(), "robot_description/urdf/simple_robot.urdf"),
+                             basePosition=[0, 0, 0.1],
+                             useFixedBase=False)
+pybullet.setGravity(0, 0, -9.81)
 
 while True:
-  pybullet.stepSimulation()
-  posAndOrn = pybullet.getBasePositionAndOrientation(subj)
+    pybullet.stepSimulation()
+    posAndOrn = pybullet.getBasePositionAndOrientation(robot_id)
 
-  # jointIndex = 1 refers to the left wheel
-  pybullet.setJointMotorControl2(bodyUniqueId=subj, jointIndex=1, controlMode=pybullet.VELOCITY_CONTROL, targetVelocity=20.0)
-  # jointIndex = 0 refers to the right wheel
-  pybullet.setJointMotorControl2(bodyUniqueId=subj, jointIndex=0, controlMode=pybullet.VELOCITY_CONTROL, targetVelocity=-20.0)
-  time.sleep(1/240)
+    # jointIndex = 1 refers to the left wheel
+    pybullet.setJointMotorControl2(bodyUniqueId=robot_id, jointIndex=1, controlMode=pybullet.VELOCITY_CONTROL,
+                                   targetVelocity=0.0)
+    # jointIndex = 0 refers to the right wheel
+    pybullet.setJointMotorControl2(bodyUniqueId=robot_id, jointIndex=0, controlMode=pybullet.VELOCITY_CONTROL,
+                                   targetVelocity=-0.0)
+    time.sleep(1 / 240)
